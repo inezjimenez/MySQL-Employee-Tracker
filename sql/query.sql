d84667b4f186d6706aab5dce22c0a6ac30e22f3a
Inez  Mercedes Jimenez <ijim9182@yahoo.com>
2022-08-26 14:46:40 -0500

    Adding department querries

--- sql/query.sql
+++ sql/query.sql
@@ -1,3 +1,5 @@
-SELECT * FROM role JOIN roles ON employee.role_id = role.id
+/*this is where we join the ids and create them together*/
+SELECT * FROM roles JOIN department ON roles.department_id = department.id;/*this is joining the department id to the roles table*/
 
-JOIN department ON role.department_id = department.id;
+/*this is joining the roles id to the employee table*/
+FROM employee JOIN roles ON employee.role_id = role.id;
--- sql/seeds.sql
+++ sql/seeds.sql
@@ -1,12 +1,24 @@
+
+USE employeeDB;
+
 INSERT INTO department(name)
-VALUES ("IT"),
-("Media");
+VALUES
+('Sales'),
+('Support'),
+('Marketing'),
+('Development');
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("GM", 100, 1),
-        ();
+VALUES
+('Sales Manager', 60000, 1),
+('Sales Associate', 40000, 1),
+('Support Manager', 65000, 2),
 
-INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES ("Bob", "John", ID, NULL),
-("Terry", "Johnson", 123, 1);
+INSERT INTO employee
+(first_name, last_name, role_id, manager_id)
+
+VALUES
+("Bob", "John", 1, NULL),
+("Terry", "Johnson", 2, 1),
+("Mike","Miller",3,2),
 
