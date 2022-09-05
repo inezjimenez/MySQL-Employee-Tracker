82cb538f8e29bb031a9aea24b3ba75c15ce49142
Inez  Mercedes Jimenez <ijim9182@yahoo.com>
2022-09-05 00:53:46 -0500

    All questions ready to be prompt

--- server.js
+++ server.js
@@ -47,13 +47,13 @@ function menu() {
                 viewAllEmployees();
                 break;
             case 'Add Employee':
-                apdateEmployee();
+                addEmployee();
                 break;
             case 'Update Employee Role':
                 updateEmployeeRole();
                 break;
             case 'View All Roles':
-                viewAllROles();
+                viewAllRoles();
                 break;
             case 'Add Role':
                 addRole();
@@ -105,11 +105,116 @@ const addEmployee = () => {
             name: 'employee_manager',
             message: 'Who is the manager of the employee?',
         },
-    ])
+    ]).then (ans => {
+        const employee_firstName = ans.employee_firstName;
+        const employee_lastName = ans.employee_lastName;
+        const employee_role = ans.employee_role;
+        const employee_manager = ans.employee_manager;
+
+        db.employee(employee_firstName, employee_lastName, employee_role, employee_manager)
+        .then (() => console.log(`Employee ${employee_firstName} ${employee_lastName} added`))
+        .then(() => menu())
+    })
 };
 
 const updateEmployeeRole = () => {
-    db.query('SELECT title, id FROM role')
-}
+    db.query('SELECT title, id FROM role', function (err, results) {
+        const roles = results.map(function (role) {
+            return {
+                input: role.title,
+                name: role.id
+            }
+        })
+        db.query('SELECT first_name, last_name, id FROM employee', function (err, results2) {
+            const employees = results2.map(function (employee) {
+                return {
+                    name: employee.first_name + " " + employee.last_name,
+                    value: employee.id
+                }
+            })
+            inquirer.prompt([{
+                type: 'list',
+                name: 'updateEmployee',
+                message: 'Which Employee would you like to update?',
+                choices: employees
+            },
+            {
+                type: 'list',
+                name: 'updateEmployeeRole',
+                message: 'What is the new Role?',
+                choices: roles
+            },
+            ]).then(res => {
+                db.query('UPDATE employee SET rolde_id = ? WHERE employee.id = ?', [res.updateRole, res.updateEmployee], function (err, results2) {
+                    menu()
+                })
+            })
+        })
+    })
+};
+
+const viewAllRoles = () => {
+    db.role()
+    .then(([role]) => {
+        console.table(role);
+    }).then (() => menu());
+};
+
+const addRole = () => {
+    inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'role_title',
+            message: 'What is the name of this role?',
+        },
+        {
+            type: 'input',
+            name: 'role_salary',
+            message: 'How much does this role pay?',
+        },
+        {
+            type: 'input',
+            name: 'role_department',
+            message: 'What department is this role found in?',
+        },
+    ]).then (ans => {
+        console.log(ans)
+        const roleTitle = ans.role_title;
+        const roleSalary = ans.role_salary;
+        const roleDepartment = ans.role_department;
+
+        db.addRole(roleTitle, roleSalary, roleDepartment)
+        .then(() => console.log (`Role ${roleTitle} added`))
+        .then(() => menu())
+    })
+};
+
+const viewAllDepartments = () => {
+    db.departments()
+    .then(([departments]) => {
+        console.table(departments);
+    }).then(() => menu());
+};
+
+const addDepartment = () => {
+    inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'department_name',
+            message: 'What is the name of your department?',
+        }
+    ]).then(ans => {
+        console.log('ANSWER', ans);
+        const departmentName = ans.department_name; 
+
+        db.addDepartment(departmentName)
+        .then(() => console.log(`Department ${departmentName} added`))
+        .then(() => menu())
+    })
+};
+
+const quit = () => {
+    process.exit()
+};
 
 
